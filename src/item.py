8f9cb562b11589985f1af2294d295be771fee69d
zixeel <strpan549@gmail.com>
2023-09-28 18:50:39 +0300

    task 3 complete in item

--- src/item.py
+++ src/item.py
@@ -56,7 +56,12 @@ class Item:
                 quantity = int(item['quantity'])
                 cls(name, price, quantity)
 
-
     @staticmethod
     def string_to_number(num):
         return int(float(num))
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self.__name}', {self.price}, {self.quantity})"
+
+    def __str__(self):
+        return self.__name
