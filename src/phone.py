263bb111b1f4deedeca2b1ee22df09c55a7aa701
zixeel <strpan549@gmail.com>
2023-09-29 18:14:18 +0300

    task 4 hotfix

--- src/phone.py
+++ src/phone.py
@@ -5,7 +5,19 @@ class Phone(Item):
 
     def __init__(self, name: str, price: float, quantity: int, number_of_sim: int):
         super().__init__(name, price, quantity)
-        self.number_of_sim = number_of_sim
+        self.__number_of_sim = number_of_sim
 
     def __repr__(self):
-        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
+        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.__number_of_sim})"
+
+    @property
+    def number_of_sim(self):
+        return self.__number_of_sim
+
+    @number_of_sim.setter
+    def number_of_sim(self, num):
+        if num > 0 and isinstance(num, int):
+            self.__number_of_sim = num
+        else:
+            raise ValueError('Количество физических SIM-карт должно быть целым числом больше нуля.')
+
